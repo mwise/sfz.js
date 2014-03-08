04b843f38d0acbec176c606a96888853f9f97369
Mark Wise <markmediadude@gmail.com>
2014-03-08 13:04:20 -0600

    added quick and dirty keyboard input for example page

--- example/main.js
+++ example/main.js
@@ -5,6 +5,10 @@ var sfzUrl = "/example/piano.mp3.sfz"
 var sfzUrl = "/example/Woodwinds - Oboe Solo.sfz"
 var sfzUrl = "/example/trumpet.sfz"
 var sfzUrl = "/example/horn-solo.mp3.sfz"
+
+var keyCodes = [65, 83, 68, 70, 71, 72, 74, 75]
+
+
 sfz.load(ac, sfzUrl, function(instrument){
   window.instrument = instrument
 
@@ -18,5 +22,22 @@ sfz.load(ac, sfzUrl, function(instrument){
 
   midiSelect.on("pitchBend", function(e){
     instrument.pitchBend(e.detail.channel, e.detail.bend)
+  })
+
+  var makeNoteOn = function(keyCode){
+  }
+
+  document.addEventListener("keydown", function(e){
+    var pitch = keyCodes.indexOf(e.keyCode)
+    if (pitch != -1) {
+      instrument.noteOn(1, 60 + pitch, 100)
+    }
+  })
+
+  document.addEventListener("keyup", function(e){
+    var pitch = keyCodes.indexOf(e.keyCode)
+    if (pitch != -1) {
+      instrument.noteOn(1, 60 + pitch, 0)
+    }
   })
 })
