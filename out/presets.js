3d30841c7963afded0f20e3b1d422329ab0c7350
Kim T <kimturley@gmail.com>
2023-03-19 16:39:54 -0700

    Updated paths

--- out/presets.js
+++ out/presets.js
@@ -1,17 +1,17 @@
 var presets = {
   "70s Synth": [
-    "/out/70s-synth/70s Organ Vibrato.sfz",
-    "/out/70s-synth/70s Organ.sfz",
-    "/out/70s-synth/70s Sawetooth.sfz",
-    "/out/70s-synth/70s Sinewave.sfz",
-    "/out/70s-synth/70s Squarewave Turned.sfz",
-    "/out/70s-synth/70s Squarewave.sfz",
-    "/out/70s-synth/70s Triangle.sfz",
-    "/out/70s-synth/70s Trumbone.sfz",
-    "/out/70s-synth/70s Trumpet.sfz",
-    "/out/70s-synth/70s Violin Vibrato.sfz",
-    "/out/70s-synth/70s Violin.sfz",
-    "/out/70s-synth/70s White Noise.sfz"
+    "/70s-synth/70s Organ Vibrato.sfz",
+    "/70s-synth/70s Organ.sfz",
+    "/70s-synth/70s Sawetooth.sfz",
+    "/70s-synth/70s Sinewave.sfz",
+    "/70s-synth/70s Squarewave Turned.sfz",
+    "/70s-synth/70s Squarewave.sfz",
+    "/70s-synth/70s Triangle.sfz",
+    "/70s-synth/70s Trumbone.sfz",
+    "/70s-synth/70s Trumpet.sfz",
+    "/70s-synth/70s Violin Vibrato.sfz",
+    "/70s-synth/70s Violin.sfz",
+    "/70s-synth/70s White Noise.sfz"
   ]
 }
 
